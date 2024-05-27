36134ddfc71e8a7021dc41376f8ee0657f332c2f
raisalsalim <raisalsalim333@gmail.com>
2024-05-27 14:56:30 +0530

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,60 @@
+provider "aws" {
+  region = "ap-south-1"  # Update with your preferred region
+}
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web"
+  description = "Allow web traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0f58b397bc5c1f2e8"  # Update with the latest Ubuntu AMI ID for your region
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.allow_web.name]
+
+  tags = {
+    Name = "flask-app-instance"
+  }
+
+  key_name = "CI-CD"  # Ensure you have a key pair created in AWS
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"  # User for connecting to the EC2 instance
+    private_key = file("/home/raisal/Downloads/CI-CD.pem")  # Path to your SSH private key file
+    host        = self.public_ip  # Use self.public_ip to dynamically fetch the public IP of the>
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y docker.io",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker"
+    ]
+  }
+}
+
+output "instance_public_dns" {
+  value = aws_instance.web.public_dns
+}
